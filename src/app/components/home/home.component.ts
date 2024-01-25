676f70ce477a9320f986f5d927966d5c64cbd44d
Jacob Yoon <jacobyoon926@gmail.com>
2024-01-25 01:05:10 -0500

    Stylings

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -21,7 +21,7 @@ export class HomeComponent implements OnInit {
   playlistLength = 10;
   parameters = [
     {
-      displayName: 'Target Acousticness',
+      displayName: 'Acousticness',
       name: 'target_acousticness',
       description: 'A confidence measure from 0 to 1 of whether the track is acoustic.',
       min: 0,
@@ -32,7 +32,7 @@ export class HomeComponent implements OnInit {
       on: false
     },
     {
-      displayName: 'Target Danceability',
+      displayName: 'Danceability',
       name: 'target_danceability',
       description: 'Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity.',
       min: 0,
@@ -43,7 +43,7 @@ export class HomeComponent implements OnInit {
       on: false
     },
     {
-      displayName: 'Target Energy',
+      displayName: 'Energy',
       name: 'target_energy',
       description: 'Energy is a measure from 0 to 1 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy.',
       min: 0,
@@ -54,7 +54,7 @@ export class HomeComponent implements OnInit {
       on: false
     },
     {
-      displayName: 'Target Instrumentalness',
+      displayName: 'Instrumentalness',
       name: 'target_instrumentalness',
       description: 'A confidence measure from 0 to 1 of whether a track contains no vocals.',
       min: 0,
@@ -65,7 +65,7 @@ export class HomeComponent implements OnInit {
       on: false
     },
     {
-      displayName: 'Target Liveness',
+      displayName: 'Liveness',
       name: 'target_liveness',
       description: 'Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live.',
       min: 0,
@@ -76,7 +76,7 @@ export class HomeComponent implements OnInit {
       on: false
     },
     {
-      displayName: 'Target Popularity',
+      displayName: 'Popularity',
       name: 'target_popularity',
       description: 'A measure of how popular a track is from 0 to 100.',
       min: 0,
@@ -87,7 +87,7 @@ export class HomeComponent implements OnInit {
       on: false
     },
     {
-      displayName: 'Target Tempo',
+      displayName: 'Tempo',
       name: 'target_tempo',
       description: 'The overall estimated tempo of a track in beats per minute (BPM).',
       min: 0,
@@ -98,7 +98,7 @@ export class HomeComponent implements OnInit {
       on: false
     },
     {
-      displayName: 'Target Valence',
+      displayName: 'Valence',
       name: 'target_valence',
       description: 'A measure from 0 to 1 describing the musical positiveness conveyed by a track.',
       min: 0,
