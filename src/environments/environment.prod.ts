07d3757dc96e2df41cd0ab7f834669691703f67c
Jacob Yoon <jacobyoon926@gmail.com>
2024-01-25 18:57:29 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  url: 'https://spotify-playlist-creator-3c0ba.web.app',
+  url: 'https://inspiredplaylists.web.app',
   SPOTIFY_CLIENT_ID: '2c753d353cdb4d4bb6f3fa5a5cd710dd',
   SPOTIFY_CLIENT_SECRET: '904efe09b73441a999f4ac9802894644',
   OPENAI_API_KEY: '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'
